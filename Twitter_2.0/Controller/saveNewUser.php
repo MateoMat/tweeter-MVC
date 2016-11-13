301e4bcb2ceefc6b1e800cc09713e82bcc0576b2
mateusz <mateusz@MacBook-Pro-mateusz.local>
2016-11-13 22:02:35 +0100

    Added Session Variables from workflow of registering user

--- Twitter_2.0/Controller/saveNewUser.php
+++ Twitter_2.0/Controller/saveNewUser.php
@@ -1,8 +1,7 @@
 <?php
-
 session_start();
-require_once '../Model/Database.php';
-require_once '../Model/User.php';
+require_once '../Model/Database.php'; //dołączona klasa Database
+require_once '../Model/User.php'; //dołączona klasa User
 
 $conn = Database::createConnection();
 
@@ -13,22 +12,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $newEmail = $_POST['newEmail'];
         $newPwd = password_hash($_POST['newPwd'], PASSWORD_BCRYPT);
 
-        $new = new User();
-        $new->setEmail($newEmail);
-        $new->setUserName($newUser);
-        $new->setPassword($newPwd);
-        echo $new->getUserName();
-        echo $new->getEmail();
-        echo $new->getPassword();
-        
-        
-        $save = $new->saveToDbUser($conn);
+        $new = new User(); //tworzy obiekt User
+        $new->setEmail($newEmail); //ustawia email obiektu
+        $new->setUserName($newUser); //ustawia imię obiektu
+        $new->setPassword($newPwd); // ustawia hasło obiektu
 
-        if ($save == true) {
-            echo "Wszystko jest o.k.";
+
+        $save = $new->saveToDbUser($conn); //zapisuje obiket User do bazy
+
+        $sql = 'SELECT * FROM `users` WHERE `email`="'.$newEmail.'"';
+        $check = $conn->query($sql); // odpytujemy, czy użytkownik jest w bazie
+        
+        if ($check) {
+
+            $password = '';
+            $name = '';
+            $mail = '';
+            $id = '';
+
+            foreach ($check as $arr) {
+                $password = $arr['password'];
+                $name = $arr['username'];
+                $mail = $arr['email'];
+                $id = $arr['id'];
+            }
+
+            $verify = password_verify($_POST['newPwd'], $password);//sprawdzamy, czy hasło jest poprawne
+            
+            if ($verify) {
+                $_SESSION['logIn'] = true;
+                $_SESSION['name'] = $name;
+                $_SESSION['email'] = $mail;
+                $_SESSION['password'] = $password;
+                $_SESSION['id'] = $id;
+                $_SESSION['wrongEmail']=false; //ustawiamy sesję na false - wszystko gra
+                header('location:../View/main.php');
+            }else{
+                $_SESSION['wrongEmail']=true; // jeżeli hasła się nie pokrywają - zwracamy false
+                header('location:../View/newUserRegisterForm.php');
+            }
+            
+            
+            
         } else {
             echo "Coś poszło nie tak";
         }
     }
 }
 
+
+    
